128c11e538af50d4a6efdd33565aa3a049bb553a
Lucas David <lucas.david@drexel.edu>
2015-04-07 11:26:14 -0300

    Replace usage of _lamp_list by lamps

--- 1.Learning/corredor.py
+++ 1.Learning/corredor.py
@@ -12,9 +12,9 @@ class Corredor:
         return self._lamp_list_
 
     def _go_jose_once(self, current):
-        for i in range(current, len(self._lamp_list_)+1, current):
-            self._lamp_list_[i-1] = not self._lamp_list_[i-1]
+        for i in range(current, len(self.lamps)+1, current):
+            self.lamps[i-1] = not self.lamps[i-1]
 
     def go_jose_go(self):
-        for i in range(1, len(self._lamp_list_)+1):
+        for i in range(1, len(self.lamps)+1):
             self._go_jose_once(i)
