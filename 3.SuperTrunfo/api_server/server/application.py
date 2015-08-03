7fe968a1fdb5e58fad118c3221faa4c68f21ad43
Ariel <arielmorelli2@gmail.com>
2015-08-03 19:39:47 -0300

    Rm files.

--- 3.SuperTrunfo/api_server/server/application.py
+++
@@ -1,6 +0,0 @@
-from server import app, router
-from . import authentication
-
-def run():
-    router.define_routes()
-    app.run(host="0.0.0.0", debug=True)
--- 3.SuperTrunfo/api_server/server/common/models.py
+++
@@ -1,2 +0,0 @@
-class BaseModel(object):
-    pk_field = None
